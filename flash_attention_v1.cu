081ff1e4ca1224778a80792ebd6e4a212fada91f
ZhenghaoChi-Felix <131735752+Felix-Zhenghao@users.noreply.github.com>
2024-06-12 11:22:42 +0800

    Update flash_attention_v1.cu

--- flash_attention_v1.cu
+++ flash_attention_v1.cu
@@ -2,6 +2,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+
 __global__
 void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d,
                     const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
